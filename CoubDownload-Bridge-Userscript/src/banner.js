42280a34478179ed721b86bae684eb97b5dbd073
Venipa <admin@venipa.net>
2020-01-15 18:58:42 +0100

    header update, prod auto header, mutation observer instead of interval

---
+++ CoubDownload-Bridge-Userscript/src/banner.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Coub-Bridge
+// @namespace    https://venipa.net
+// @version      0.1.7
+// @description  try to take over the world!
+// @author       Venipa
+// @match        https://*.coub.com/*
+// @match        https://coub.com/*
+// @grant        none
+// @license GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// ==/UserScript==
+// ==OpenUserJS==
+// @author Venipa
+// ==/OpenUserJS==
